6cf323ee3881c1a162064220a2b47ec049efaf1f
John Schroeder <john@schroedernet.software>
2016-10-03 12:55:12 -0500

    Add append/prepend path functions

---
+++ home/.bashrc.d/path.sh
@@ -0,0 +1,19 @@
+append_path_if_exists() {
+  if [[ -d $1 ]]
+  then
+    export PATH="$PATH:$1"
+    return 0
+  else
+    return 1
+  fi
+}
+
+prepend_path_if_exists() {
+  if [[ -d $1 ]]
+  then
+    export PATH="$1:$PATH"
+    return 0
+  else
+    return 1
+  fi
+}

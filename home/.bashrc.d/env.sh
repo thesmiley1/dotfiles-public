0c490298d89a4e454e87f5fd598140448df023af
John Schroeder <john@schroedernet.software>
2018-05-07 18:23:35 -0500

    Move go.sh to env.sh
    PATH is an environment variable and makes sense to go in env.sh...

--- home/.bashrc.d/env.sh
+++ home/.bashrc.d/env.sh
@@ -13,6 +13,15 @@ export LESS="-iRx2"
 # less is more, right?
 export PAGER="less"
 
+# append GOBIN or GOPATH/bin to PATH
+if which go > /dev/null 2>&1; then
+  if [[ -n $(go env GOBIN) ]]; then
+    PATH="$PATH:$(go env GOBIN)"
+  else
+    PATH="$PATH:$(go env GOPATH)/bin"
+  fi
+fi
+
 # a magic incation if there ever was one
 export PS1="\[\033[0;35m\]\u\[\033[0;30m\]@\[\033[0;33m\]\h\[\033[0;30m\]:\[\033[0;34m\]\w\[\033[0;32m\]\$(__git_ps1 '(%s)')\[\033[0m\]\\\$ "
 
--- home/.bashrc.d/go.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-if [[ `which go 2> /dev/null` ]]; then
-  if [[ ! -z `go env GOBIN` ]]; then
-    PATH="$PATH:$(go env GOBIN)"
-  else
-    PATH="$PATH:$(go env GOPATH)/bin"
-  fi
-fi

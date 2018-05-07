58211de1600e4d5b12699209b134c84438e7d57d
John Schroeder <john@schroedernet.software>
2018-05-07 18:41:04 -0500

    Add whitespace to alias.sh

--- home/.bashrc.d/alias.sh
+++ home/.bashrc.d/alias.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
 alias grep='grep --color=auto'
+
 alias ls='ls --color=always -ahl'
+
 alias ssv='vagrant ssh'
+
 alias vbox='VBoxManage'
+
 alias vh='vagrant halt'
+
 alias vs='vagrant status'
+
 alias vu='vagrant up'

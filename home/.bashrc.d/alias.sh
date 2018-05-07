a71da3b73feee4c13cf5f0b1f52e1707bfc10dd4
John Schroeder <john@schroedernet.software>
2018-05-06 23:14:44 -0500

    Always output color from `ls`

--- home/.bashrc.d/alias.sh
+++ home/.bashrc.d/alias.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 alias grep='grep --color=auto'
-alias ls='ls --color=auto -ahl'
+alias ls='ls --color=always -ahl'
 alias ssv='vagrant ssh'
 alias vbox='VBoxManage'
 alias vh='vagrant halt'

b60cf09b7c468c4c0a7b7fc68051678d007aa9e1
John Schroeder <john@schroedernet.software>
2019-12-04 03:26:56 -0600

    bash:  Fix typo in `follow` comment

--- home/.config/bash/public/follow.sh
+++ home/.config/bash/public/follow.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 
-# follow symlinks of give directory (or current working directory, if not
+# follow symlinks of given directory (or current working directory, if not
 # provided) and `cd` to it
 function follow() {
   local path="${1:-.}"

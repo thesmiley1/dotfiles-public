ecf1f4770cf3b939d1264fef7c0c84ca49f23cfc
John Schroeder <john@schroedernet.software>
2020-06-19 06:31:41 -0700

    zsh:  Make plugin loading Ubuntu-compatible
    Additionally, skip loading plugins that are not found.
    
    Arch packages install zsh plugins to
    `/usr/share/zsh/plugins/$plugin/$plugin.zsh`, but Ubuntu (20.04)
    packages intsll zsh plugins to `/usr/share/$plugin/$plugin.zsh`.  Now
    plugin loading is attempted from `/usr/share/zsh/plugins` if it exists,
    or `/usr/share` otherwise.

--- home/.config/zsh/public.zsh
+++ home/.config/zsh/public.zsh
@@ -1,6 +1,21 @@
 #! /usr/bin/env zsh
 
-local plugins_dir="/usr/share/zsh/plugins"
+local plugin
+
+local plugins=(
+  "zsh-autosuggestions"
+  "zsh-syntax-highlighting"
+
+  "zsh-history-substring-search"
+)
+
+# Arch packages install zsh plugins in `/usr/share/zsh/plugins`, but Ubuntu
+# (20.04) installs them to `/usr/share`.
+local plugins_dir="/usr/share"
+if [[ -d "${plugins_dir}/zsh/plugins" ]]; then
+  plugins_dir="${plugins_dir}/zsh/plugins"
+fi
+
 local source_dir="${XDG_CONFIG_HOME:-${HOME}/.config}/zsh/public"
 
 source "${source_dir}/options.zsh"
@@ -11,10 +26,11 @@ source "${source_dir}/global.zsh"
 source "${source_dir}/git-prompt.zsh"
 source "${source_dir}/prompt.zsh"
 
-source "${plugins_dir}/zsh-autosuggestions/zsh-autosuggestions.zsh"
-source "${plugins_dir}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
-
-source "${plugins_dir}/zsh-history-substring-search/zsh-history-substring-search.zsh"
+for plugin in "${plugins[@]}"; do
+  if [[ -e "${plugins_dir}/${plugin}/${plugin}.zsh" ]]; then
+    source "${plugins_dir}/${plugin}/${plugin}.zsh"
+  fi
+done
 
 source "${source_dir}/alias.zsh"
 source "${source_dir}/completion.zsh"
@@ -24,5 +40,7 @@ source "${source_dir}/zutil.zsh"
 
 source "${source_dir}/lgr.zsh"
 
+unset plugin
+unset plugins
 unset plugins_dir
 unset source_dir
